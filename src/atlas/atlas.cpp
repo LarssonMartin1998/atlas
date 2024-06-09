4d170b8bf75bc9c5ae8271d2d31e9bbcf35a784a
Martin Larsson <larssonmartin1998@gmail.com>
2024-06-09 14:42:10 +0200

    Add some temp raylib code to cmake and atlas.cpp to verify that it works.

--- src/atlas/atlas.cpp
+++ src/atlas/atlas.cpp
@@ -2,6 +2,13 @@
 
 #include <iostream>
 
+#include "raylib.h"
+
 namespace atlas {
-auto lib_entry() -> void { std::cout << "Hello from Atlas!\n"; }
+auto lib_entry() -> void {
+  constexpr int screen_width = 800;
+  constexpr int screen_height = 600;
+  InitWindow(screen_width, screen_height, "Atlas");
+  std::cout << "Hello from Atlas!\n";
+}
 } // namespace atlas
