04ea89e3344370b4b7b2a99c2b6083f30a0463b5
Martin Larsson <larssonmartin1998@gmail.com>
2024-11-14 01:21:48 +0100

    Add ITickable, attach it to any class that wants ticking capabilities

---
+++ include/atlas/core/ITickable.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace atlas::core {
+class ITickable {
+  public:
+    virtual ~ITickable() = default;
+
+    ITickable(const ITickable &) = delete;
+    auto operator=(const ITickable &) -> ITickable & = delete;
+
+    ITickable(ITickable &&) = delete;
+    auto operator=(ITickable &&) -> ITickable & = delete;
+
+    virtual auto tick() -> void = 0;
+    [[nodiscard]] virtual auto get_tick_rate() const -> unsigned = 0;
+
+  protected:
+    ITickable() = default;
+};
+} // namespace atlas::core
