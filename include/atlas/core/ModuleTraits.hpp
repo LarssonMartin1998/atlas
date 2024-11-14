373422546548fb4ea0695c614ab4f56ea9b15a1c
Martin Larsson <larssonmartin1998@gmail.com>
2024-11-14 01:19:20 +0100

    Refactor module traits so it's easier to add new modules by using a macro.

--- include/atlas/core/ModuleTraits.hpp
+++ include/atlas/core/ModuleTraits.hpp
@@ -2,12 +2,24 @@
 
 #include "EModules.hpp"
 
-namespace atlas::core {
-class ECSModule;
-
+namespace atlas {
+namespace core {
 template <typename T> struct ModuleTraits;
+}
 
-template <> struct ModuleTraits<ECSModule> {
-    static constexpr EModules module_enum = EModules::ECS;
-};
-} // namespace atlas::core
+#define MODULE_TRAIT(ModuleClassName, module_namespace_name)                   \
+    namespace module_namespace_name {                                          \
+    class ModuleClassName;                                                     \
+    }                                                                          \
+    namespace core {                                                           \
+    template <> struct ModuleTraits<module_namespace_name::ModuleClassName> {  \
+        static constexpr EModules module_enum = EModules::ModuleClassName;     \
+    };                                                                         \
+    }
+
+// Use the macro to define a forward declaration and a ModuleTraits
+// specialization
+MODULE_TRAIT(Hephaestus, hephaestus)
+
+#undef MODULE_TRAIT
+} // namespace atlas
