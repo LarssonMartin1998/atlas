b64abf660992270e3ac4d6aebc05d76660974526
Martin Larsson <larssonmartin1998@gmail.com>
2024-11-29 14:47:06 +0100

    Add Component interface.

---
+++ modules/hephaestus/include/hephaestus/IComponent.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace atlas::hephaestus {
+class IComponent {
+  public:
+    virtual ~IComponent() = default;
+
+    IComponent(const IComponent &) = default;
+    auto operator=(const IComponent &) -> IComponent & = default;
+
+    IComponent(IComponent &&) = delete;
+    auto operator=(IComponent &&) -> IComponent & = delete;
+
+  protected:
+    IComponent() = default;
+};
+} // namespace atlas::hephaestus
