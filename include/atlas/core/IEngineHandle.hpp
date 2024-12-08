cc1bcfd34d5a606b870ac4046eb294b300fc65f3
Martin Larsson <larssonmartin1998@gmail.com>
2024-12-08 16:31:05 +0100

    Add IEngineHandle, interface for retrieving the engine

---
+++ include/atlas/core/IEngineHandle.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace atlas::core {
+class IEngine;
+}
+
+namespace atlas::core {
+class IEngineHandle {
+  public:
+    virtual ~IEngineHandle() = default;
+
+    IEngineHandle(const IEngineHandle &) = delete;
+    auto operator=(const IEngineHandle &) -> IEngineHandle & = delete;
+
+    IEngineHandle(IEngineHandle &&) = delete;
+    auto operator=(IEngineHandle &&) -> IEngineHandle & = delete;
+
+    [[nodiscard]] virtual auto get_engine() const -> IEngine & = 0;
+
+  protected:
+    IEngineHandle() = default;
+};
+} // namespace atlas::core
