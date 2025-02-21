91c3ed287c01adab46b37b3c8e11096979074128
Martin Lasson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-02-21 01:28:40 +0100

    Add support for aarch64/x86_64 darwin/linux

--- flake.nix
+++ flake.nix
@@ -3,41 +3,48 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      stdenv = pkgs.llvmPackages.stdenv;
-    in
     {
-      packages.${system}.default = stdenv.mkDerivation {
-        pname = "atlas";
-        version = "0.1.0";
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        stdenv = pkgs.llvmPackages.stdenv;
+      in
+      {
+        packages.default = stdenv.mkDerivation {
+          pname = "atlas";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            clang-tools
+            ninja
+            cmake
+            gtest
+          ];
+
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+            "-DBUILD_TESTS=ON"
+            "-DENABLE_INSTALL=ON"
+          ];
+
+          doCheck = true;
+          checkPhase = ''
+            ctest --output-on-failure
+          '';
+        };
+
+        # Expose the entire source for the package so that it can be added as a cmake subdirectory in game projects using atlas and nix flakes to build.
         src = ./.;
-
-        nativeBuildInputs = with pkgs; [
-          clang-tools
-          ninja
-          cmake
-          gtest
-        ];
-
-        cmakeFlags = [
-          "-DCMAKE_BUILD_TYPE=Release"
-          "-DBUILD_TESTS=ON"
-          "-DENABLE_INSTALL=ON"
-        ];
-
-        doCheck = true;
-        checkPhase = ''
-          ctest --output-on-failure
-        '';
-      };
-
-      # Expose the entire source for the package so that it can be added as a cmake subdirectory in game projects using atlas and nix flakes to build.
-      src = ./.;
-    };
+      }
+    );
 }
