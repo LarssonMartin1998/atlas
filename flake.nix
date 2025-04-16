6567e723286a57af2390566af0264af1dab6df96
Martin Lasson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-04-17 00:19:13 +0200

    Setup vulkan/moltenvk dependencies in nix and cmake (only tested on mac         so far)

--- flake.nix
+++ flake.nix
@@ -19,10 +19,18 @@
         stdenv = pkgs.llvmPackages.stdenv;
       in
       {
-        packages.default = stdenv.mkDerivation {
+        packages.atlas = stdenv.mkDerivation {
           pname = "atlas";
           version = "0.1.0";
           src = ./.;
+
+          buildInputs =
+            with pkgs;
+            [
+              vulkan-headers
+              vulkan-loader
+            ]
+            ++ lib.optional stdenv.isDarwin [ moltenvk ];
 
           nativeBuildInputs = with pkgs; [
             clang-tools
@@ -37,14 +45,43 @@
             "-DENABLE_INSTALL=ON"
           ];
 
+          # Needed on Linux: libvulkan is loaded with dlopen()
+          # so it is *not* seen by patchelf → keep the RPATH.
+          dontPatchELF = stdenv.isLinux; # :contentReference[oaicite:0]{index=0}
+
           doCheck = true;
           checkPhase = ''
             ctest --output-on-failure
           '';
         };
 
+        # devShells.default = pkgs.mkShell {
+        #   buildInputs =
+        #     with pkgs;
+        #     [
+        #       clang
+        #       ninja
+        #       cmake
+        #       vkLoader
+        #       pkgs.vulkan-headers
+        #     ]
+        #     ++ lib.optionals stdenv.isDarwin [
+        #       moltenvk
+        #       pkgs.vulkan-validation-layers
+        #     ];
+        #
+        #   shellHook = ''
+        #     if [[ "$(uname)" == "Darwin" ]]; then
+        #       export VK_ICD_FILENAMES=${pkgs.moltenvk}/share/vulkan/icd.d/MoltenVK_icd.json
+        #       export VK_LAYER_PATH=${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d
+        #     fi
+        #     echo "🔧  Vulkan SDK ready – happy hacking!"
+        #   '';
+        # };
         # Expose the entire source for the package so that it can be added as a cmake subdirectory in game projects using atlas and nix flakes to build.
         src = ./.;
+
+        packages.default = self.packages.${system}.atlas;
       }
     );
 }
