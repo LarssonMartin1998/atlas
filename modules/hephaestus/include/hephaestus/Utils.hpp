9cdfb9e1ce95275c7f92dc217e1f387f4023a9dc
Martin Larsson <larssonmartin1998@gmail.com>
2025-05-10 16:34:45 +0000

    Add functions for comparing and checking overlappings signatures

--- modules/hephaestus/include/hephaestus/Utils.hpp
+++ modules/hephaestus/include/hephaestus/Utils.hpp
@@ -14,4 +14,11 @@ auto make_component_type_signature() -> std::vector<std::type_index> {
     std::ranges::sort(type_indices);
     return type_indices;
 }
+
+auto compare_signatures(const std::vector<std::type_index>& lhs,
+                        const std::vector<std::type_index>& rhs) -> bool;
+
+auto are_signatures_overlapping(const std::vector<std::type_index>& lhs,
+                                const std::vector<std::type_index>& rhs)
+    -> bool;
 } // namespace atlas::hephaestus
